5a89e50c84fa5b80677d8da34b52955ab78a29b5
Marissa Zhang <yzhang2587@wisc.edu>
2025-01-27 21:58:50 -0600

    fix typo in fastqc2

--- fastqc2.sh
+++ fastqc2.sh
@@ -5,4 +5,4 @@ multiqc_staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/RSEM/Results/M
 fastqc $results_staging_prefix/$1_1P.fq.gz $results_staging_prefix/$1_2P.fq.gz -t 4
 
 mv $1_1P_fastqc.zip $multiqc_staging_prefix/
-mv $1__2P_fastqc.zip $multiqc_staging_prefix/
+mv $1_2P_fastqc.zip $multiqc_staging_prefix/
