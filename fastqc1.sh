a05711b6f254afddbfd1c9bf080399370780de34
Marissa Zhang <yzhang2587@wisc.edu>
2025-01-28 11:28:45 -0600

    Copy /staging files to local to avoid leaving unwanted results in /staging

--- fastqc1.sh
+++ fastqc1.sh
@@ -2,7 +2,10 @@
 staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/RSEM
 multiqc_staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/RSEM/Results/MultiQC
 
-fastqc $staging_prefix/$1_R1_001.fastq.gz $staging_prefix/$1_R2_001.fastq.gz -t 4
+cp $staging_prefix/$1_R1_001.fastq.gz $staging_prefix/$1_R2_001.fastq.gz ./
+fastqc $1_R1_001.fastq.gz $1_R2_001.fastq.gz -t 4
 
 mv $1_R1_001_fastqc.zip $multiqc_staging_prefix/
 mv $1_R2_001_fastqc.zip $multiqc_staging_prefix/
+
+rm $1_R1_001.fastq.gz $1_R2_001.fastq.gz
--- fastqc2.sh
+++ fastqc2.sh
@@ -2,7 +2,10 @@
 results_staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/RSEM/Results
 multiqc_staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/RSEM/Results/MultiQC
 
-fastqc $results_staging_prefix/$1_1P.fq.gz $results_staging_prefix/$1_2P.fq.gz -t 4
+cp $results_staging_prefix/$1_1P.fq.gz $results_staging_prefix/$1_2P.fq.gz ./
+fastqc $1_1P.fq.gz $1_2P.fq.gz -t 4
 
 mv $1_1P_fastqc.zip $multiqc_staging_prefix/
 mv $1_2P_fastqc.zip $multiqc_staging_prefix/
+
+rm $1_1P.fq.gz $1_2P.fq.gz
